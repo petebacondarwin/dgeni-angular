54d7f4970e6a25cf5e0bdfc5c93da0e900801b22
Peter Bacon Darwin <pete@bacondarwin.com>
2014-10-15 22:28:17 +0100

    feat(templates): add some angular-material goodness

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 var gulp = require('gulp');
 var Dgeni = require('dgeni');
 var del = require('del');
+var bower = require('bower');
 
-gulp.task('dgeni', ['clean'], function() {
+gulp.task('dgeni', function() {
   try {
     var dgeni = new Dgeni([require('./docs/dgeni-conf')]);
     return dgeni.generate();
@@ -10,6 +11,22 @@ gulp.task('dgeni', ['clean'], function() {
     console.log(x.stack);
     throw x;
   }
+});
+
+gulp.task('bower', function() {
+  var bowerTask = bower.commands.install();
+  bowerTask.on('log', function (result) {
+    log('bower:', result.id, result.data.endpoint.name);
+  });
+  bowerTask.on('error', function(error) {
+    log(error);
+  });
+  return bowerTask;
+});
+
+gulp.task('assets', ['bower'], function() {
+  return gulp.src('bower_components/**/*')
+    .pipe(gulp.dest('build/lib'));
 });
 
 gulp.task('clean', function(done) {
