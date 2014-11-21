e85cf6c3d8264cf6141cfbbe2b97dc674b767386
Peter Bacon Darwin <pete@bacondarwin.com>
2014-11-21 13:06:16 +0000

    chore(dgeni-conf): tidy up file

--- docs/dgeni-conf.js
+++ docs/dgeni-conf.js
@@ -1,41 +1,28 @@
 // Canonical path provides a consistent path (i.e. always forward slashes) across different OSes
 var path = require('canonical-path');
-
 var Package = require('dgeni').Package;
 
-// Create and export a new Dgeni package called dgeni-example. This package depends upon
-// the jsdoc and nunjucks packages defined in the dgeni-packages npm module.
+
 module.exports = new Package('dgeni-example', [
   require('dgeni-packages/angularjs'),
   require('dgeni-packages/jsdoc'),
   require('dgeni-packages/nunjucks')
 ])
 
-// Configure our dgeni-example package. We can ask the Dgeni dependency injector
-// to provide us with access to services and processors that we wish to configure
+
 .config(function(log, readFilesProcessor, writeFilesProcessor) {
 
-  // Set logging level
   log.level = 'info';
 
-  // Specify the base path used when resolving relative paths to source and output files
   readFilesProcessor.basePath = path.resolve(__dirname, '..');
-
-  // Specify collections of source files that should contain the documentation to extract
   readFilesProcessor.sourceFiles = [
-    {
-      // Process all js files in `src` and its subfolders ...
-      include: 'src/**/*.js',
-      // When calculating the relative path to these files use this as the base path.
-      // So `src/foo/bar.js` will have relative path of `foo/bar.js`
-      basePath: 'src'
-    }
+    { include: 'src/**/*.js', basePath: 'src' }
   ];
 
-  // Specify where the writeFilesProcessor will write our generated doc files
   writeFilesProcessor.outputFolder  = 'build';
 
 })
+
 
 .config(function(templateFinder, templateEngine) {
 
@@ -45,8 +32,8 @@ module.exports = new Package('dgeni-example', [
     variableEnd: '$}'
   };
 
-  // Add a folder to search for our own templates to use when rendering docs
-  templateFinder.templateFolders.unshift(path.resolve(__dirname, 'templates'));
+  templateFinder.templateFolders
+      .unshift(path.resolve(__dirname, 'templates'));
 
   templateFinder.templatePatterns = [
     '${ doc.template }',
@@ -55,4 +42,13 @@ module.exports = new Package('dgeni-example', [
     '${ doc.docType }.template.html',
     'common.template.html'
   ];
+})
+
+
+.config(function(getLinkInfo) {
+  getLinkInfo.relativeLinks = true;
 });
+
+
+
+
